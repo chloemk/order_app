9d65302c3e59fb689bd1c6ce9e76c2e3a50ffb9b
chloemk <minsung.chloe.kim@gmail.com>
2022-09-13 14:10:54 +0900

    add common colors

---
+++ lib/common/const/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+// 색상 0xFF는 투명도가 없는 색상
+const PRIMARY_COLOR = Color(0xFF22A45D);
+const BODY_TEXT_COLOR = Color(0xFF868686);
+const INPUT_BG_COLOR = Color(0xFFFBFBFB);
+const INPUT_BORDER_COLOR = Color(0xFFF2F2F2);
