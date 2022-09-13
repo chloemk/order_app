582cfc82fc7ea28cdfd0f7a8ff0fa142e55150c9
chloemk <minsung.chloe.kim@gmail.com>
2022-09-13 14:57:09 +0900

    add custom form field

---
+++ lib/common/component/custom_text_form_field.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:order_app/common/const/colors.dart';
+
+class CustomTextFormField extends StatelessWidget {
+  final String? hintText;
+  final String? errorText;
+  final bool obscureText;
+  final bool autofocus;
+  final ValueChanged<String>? onChanged;
+
+  const CustomTextFormField({
+    Key? key,
+    this.hintText,
+    this.errorText,
+    this.obscureText = false,
+    this.autofocus = false,
+    required this.onChanged,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // border의 기본값 UnderlineInputBorder
+    final baseBorder = OutlineInputBorder(
+      // 실제 테두리
+      borderSide: BorderSide(
+        color: INPUT_BORDER_COLOR,
+        width: 1.0,
+      ),
+    );
+
+    return TextFormField(
+      cursorColor: PRIMARY_COLOR,
+      obscureText: obscureText,
+      autofocus: autofocus,
+      onChanged: onChanged,
+      decoration: InputDecoration(
+        contentPadding: EdgeInsets.all(20),
+        hintText: hintText,
+        errorText: errorText,
+        hintStyle: TextStyle(
+          color: BODY_TEXT_COLOR,
+          fontSize: 14.0,
+        ),
+        fillColor: INPUT_BG_COLOR,
+        filled: true,
+        // 모든 Input 상태의 기본 스타일 세팅
+        border: baseBorder,
+        // copyWith는 객체의 모든 특성 모두 유지한 상태에서 변경한 것만 변경할 수 있음
+        focusedBorder: baseBorder.copyWith(
+            borderSide: baseBorder.borderSide.copyWith(color: PRIMARY_COLOR)),
+      ),
+    );
+  }
+}
